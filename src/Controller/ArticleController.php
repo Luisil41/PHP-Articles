98fcbfb77d5f096bdd9654991068d84de1a1c640
Luis Isarría <luisisarria@MacBook-Air-de-Luis.local>
2021-09-04 15:46:56 +0200

    FEAT: articles working

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -25,6 +25,52 @@ class ArticleController extends AbstractController
 
         $articles= $this->getDoctrine()->getRepository(Article::class)->findAll();
   
-        return $this->render('article/index.html.twig', array('articles' => $articles));
+        return $this->render('articles/index.html.twig', array('articles' => $articles));
+      }
+
+      /**
+     * @Route("/article/new", name="new_article")
+     * Method({"GET", "POST"})
+     */
+    public function new(Request $request) {
+      $article = new Article();
+
+      $form = $this->createFormBuilder($article)
+        ->add('title', TextType::class, array('attr' => array('class' => 'form-control')))
+        ->add('body', TextareaType::class, array(
+          'required' => false,
+          'attr' => array('class' => 'form-control')
+        ))
+        ->add('save', SubmitType::class, array(
+          'label' => 'Create',
+          'attr' => array('class' => 'btn btn-primary mt-3')
+        ))
+        ->getForm();
+
+      $form->handleRequest($request);
+
+      if($form->isSubmitted() && $form->isValid()) {
+        $article = $form->getData();
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($article);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('article_list');
+      }
+
+      return $this->render('articles/new.html.twig', array(
+        'form' => $form->createView()
+      ));
+    }
+
+
+    /**
+     * @Route("/article/{id}", name="article_show")
+     */
+    public function show($id) {
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+  
+        return $this->render('articles/show.html.twig', array('article' => $article));
       }
 }
